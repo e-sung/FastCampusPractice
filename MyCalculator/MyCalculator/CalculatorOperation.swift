e9ae6e8c8c035558e4776e994c1d1340afd1dca4
esung <dev.esung@gmail.com>
2017-09-19 12:31:22 +0900

    modified:   MyCalculator/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   MyCalculator/CalculatorOperation.swift

---
+++ MyCalculator/MyCalculator/CalculatorOperation.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorOperation.swift
+//  MyCalculator
+//
+//  Created by esung on 2017. 9. 19..
+//  Copyright © 2017년 esung. All rights reserved.
+//
+
+import Foundation
+enum CalculatorOperation{
+    case addition
+    case substraction
+    case multiplication
+    case division
+}
